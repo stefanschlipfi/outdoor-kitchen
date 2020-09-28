55f13aa2f39ca51d746a4ca5c58063d9b15931b0
Stefan Schlipfinger <stefan.schlipfinger@wien.gv.at>
2020-09-28 16:21:17 +0200

    copied from server-monitor v1 sensor.py for gpio

---
+++ backend/flask_app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'J$eEV§0uzGzjRxTC$DM2LKy!'
+
+#websocket
+from flask_socketio import SocketIO, emit
+socketio = SocketIO(app,cors_allowed_origins='*',async_mode="threading")
+
+#import logger
+from utils import *
+logger_name = 'flask_views'
+init_logger(loglevel='DEBUG',filename='flask_views.log',logger_name=logger_name)
+import logging
+logger = logging.getLogger(logger_name)
+
+#import CPU Thread from thread.py
+from thread import TempThread
+
+#Websocket Way
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect', namespace='/ws_sensors')
+def test_connect():
+    logger.debug("Connected with socket.io")
+    thread = TempThread(socketio=socketio,namespace='/ws_sensors')
+    thread.start()
+
+@socketio.on('disconnect', namespace='/ws_sensors')
+def test_disconnect():
+    logger.debug("Client disconnected")
+
+
+if __name__ == "__main__":
+   
+    import eventlet
+    eventlet.monkey_patch()
+
+    socketio.run(app)
+
+    
---
+++ backend/sensor.py
@@ -0,0 +1,47 @@
+from utils import *
+import psutil
+
+init_logger(filename='sensor.log',stream=False,logger_name='sensor')
+import logging
+logger = logging.getLogger('sensor')
+
+#dht sensor
+import Adafruit_DHT
+
+class Sensor():
+    def __init__(self):
+        self.DHT_Sensor = Adafruit_DHT.DHT22
+        self.config = load_conf()['sensors']
+
+    def load_senors(self):
+        """
+        load sensors from config
+        return dict
+        """
+        device_dict = {}
+
+        for sensor_type,sensor_list in self.config.items():
+            if sensor_type == 'DHT22':
+                dht_dict = device_dict.get('DHT22')
+                if not dht_dict:
+                    dht_dict = {}
+
+                for sensor in sensor_list:
+                    sensor_dict = self.load_dht22(sensor.gpio_port)
+                    sensor_dict.update({'name':sensor.name})
+                    dht_dict.update({sensor.name:sensor_dict})
+                device_dict.update({'DHT22':dht_dict})
+
+        return device_dict
+
+
+    def load_dht22(self,gpio_port):
+        """
+        @GPIO Port (int)
+        read dht22 sensor
+        return dict
+        """
+        humidity,temp = Adafruit_DHT.read_retry(self.DHT_Sensor,gpio_port)
+        humidity = format(humidity, '.2f')
+        temp = format(temp, '.2f')
+        return {'humidity':humidity,'temperature':temp,'gpio_port':gpio_port}
